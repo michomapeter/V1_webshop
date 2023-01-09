3d8d5cb6c9bb6dc3d8f8817e2803ea918aebf6ce
Peter michoma <pmichoma@kabarak.ac.ke>
2023-01-09 15:48:06 -0500

    basic layout of the application

---
+++ appstore/src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: './products-header.component.html',
+  styleUrls: ['./products-header.component.css']
+})
+export class ProductsHeaderComponent implements OnInit {
+  sort ='desc';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSortUpdated(newSort: string): void{
+    this.sort = newSort;
+  }
+
+}
